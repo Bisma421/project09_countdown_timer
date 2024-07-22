6b69da22d82c1a1a13a5f85c5da5d04237221c59
Bisma421 <bismagul421@gmail.com>
2024-07-22 13:11:08 +0500

    Cli based countdown timer

---
+++ index.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
+import chalk from "chalk";
+console.log(chalk.greenBright.bold `\n\tCountdown Timer\n`);
+const res = await inquirer.prompt({
+    name: "userInput",
+    type: "number",
+    message: "please enter the amount of seconds:",
+    validate: (input) => {
+        if (typeof input === "string") {
+            return "You must provide a valid numeric value";
+        }
+        else if (input && input > 60) {
+            return "Seconds must be in 60";
+        }
+        else {
+            return true;
+        }
+    }
+});
+let input = res.userInput;
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval(() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log("Timer has expired");
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }, 1000);
+}
+startTime(input);
